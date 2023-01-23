4b67c91df80ae16a9d5e2c07cd387598f8491811
Leonardo Martins <leonardogm85@gmail.com>
2023-01-23 20:29:40 -0300

    feat: create the promotion controller

---
+++ RealTimePromotions.Api/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealTimePromotions.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Subscribe()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
--- RealTimePromotions.Api/Program.cs
+++ RealTimePromotions.Api/Program.cs
@@ -2,9 +2,19 @@ using RealTimePromotions.Api.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
 builder.Services.AddSignalR();
 
 var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Subscribe}");
 
 app.MapHub<PromotionsHub>("/PromotionsHub");
 
