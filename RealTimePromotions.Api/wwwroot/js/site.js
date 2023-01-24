65ac93bf1e8a6fdd24915589d3ed8ae713ff3bfd
Leonardo Martins <leonardogm85@gmail.com>
2023-01-23 22:09:43 -0300

    feat: create the functionality register promotion

--- RealTimePromotions.Api/wwwroot/js/site.js
+++ RealTimePromotions.Api/wwwroot/js/site.js
@@ -1,1 +1,29 @@
-﻿
+﻿const connection = new signalR.HubConnectionBuilder().withUrl('/PromotionsHub').build();
+
+connection.start().then(function () {
+    console.info('Connected!')
+}).catch(function (err) {
+    console.error(err);
+});
+
+const register = document.getElementById('register');
+
+register?.addEventListener('click', function () {
+    const company = document.getElementById('company').value;
+    const description = document.getElementById('description').value;
+    const rule = document.getElementById('rule').value;
+    const address = document.getElementById('address').value;
+
+    const promotion = {
+        company,
+        description,
+        rule,
+        address
+    };
+
+    connection.invoke('Register', promotion).then(function () {
+        console.info('Registered Successfully!');
+    }).catch(function (err) {
+        console.error(err);
+    });
+});
