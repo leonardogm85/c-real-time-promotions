f095708ba4c666453cde625b99965fbab785502d
Leonardo Martins <leonardogm85@gmail.com>
2023-01-24 22:06:38 -0300

    feat: update promotion list layout

--- RealTimePromotions.Api/wwwroot/js/site.js
+++ RealTimePromotions.Api/wwwroot/js/site.js
@@ -14,7 +14,41 @@ connection.on('RegisteredSuccessfully', () => {
 });
 
 connection.on('ReceivePromotion', (promotion) => {
-    console.info(promotion);
+    const containerList = document.getElementById('promotion-list');
+
+    const containerItem = document.createElement('div');
+    containerItem.setAttribute('class', 'promotion-item');
+
+    const containerDescription = document.createElement('div');
+    containerDescription.setAttribute('class', 'promotion-description');
+
+    const promotionCompany = document.createElement('h1');
+    promotionCompany.innerText = promotion.company;
+
+    const promotionDescription = document.createElement('p');
+    promotionDescription.innerText = promotion.description;
+
+    const promotionRule = document.createElement('p');
+    promotionRule.innerText = promotion.rule;
+
+    const containerButton = document.createElement('div');
+    containerButton.setAttribute('class', 'promotion-button');
+
+    const promotionAddress = document.createElement('a');
+    promotionAddress.setAttribute('href', promotion.address);
+    promotionAddress.setAttribute('target', '_blank');
+    promotionAddress.innerText = 'Take';
+
+    containerDescription.appendChild(promotionCompany);
+    containerDescription.appendChild(promotionDescription);
+    containerDescription.appendChild(promotionRule);
+
+    containerButton.appendChild(promotionAddress);
+
+    containerItem.appendChild(containerDescription);
+    containerItem.appendChild(containerButton);
+
+    containerList.appendChild(containerItem);
 });
 
 const register = document.getElementById('register');
